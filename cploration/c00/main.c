fb9f43aa2b2686b42ea4ba994b09182bb9f51b62
Liam Colburn <colburnw@oregonstate.edu>
2023-09-29 13:54:53 -0700

    Create cploration directory and create hello world

---
+++ cploration/c00/main.c
@@ -0,0 +1,13 @@
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * [NAME] Liam Colburn  
+ * [TERM] FALL 2023  
+ *****************************************/
+#include <stdio.h>
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
