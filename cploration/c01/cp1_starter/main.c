cd3622777337c6cf7da7cddaa74deb0fa6b71b29
Liam Colburn <colburnw@oregonstate.edu>
2023-10-07 11:59:05 -0700

    Fix fib() recursion

--- cploration/c01/cp1_starter/main.c
+++ cploration/c01/cp1_starter/main.c
@@ -43,8 +43,10 @@ int fib(int n)
 {
     if (n == 0) {
         return 0;
+    } else if (n == 1) {
+        return 1;
     } else {
-        return fib(n) + fib(n - 1);
+        return fib(n-1) + fib(n-2);
     }
 }
  
