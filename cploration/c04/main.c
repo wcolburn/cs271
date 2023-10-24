a91a293f37e48a594f4dbc0b0c5c1442c2566474
Liam Colburn <colburnw@oregonstate.edu>
2023-10-24 11:00:31 -0700

    Init project 4

---
+++ cploration/c04/main.c
@@ -0,0 +1,17 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] $YOUR_NAME$
+ * [TERM] FALL $YEAR$
+ * 
+ ****************************************/
+#include "parser.h"
+
+int main(int argc, const char *argv[])
+{		
+
+	// your code here
+			
+}
+
+
---
+++ cploration/c04/parser.c
@@ -0,0 +1,37 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] $YOUR_NAME$
+ * [TERM] FALL $YEAR$
+ * 
+ ****************************************/
+#include "parser.h"
+
+/* Function: strip
+ * -------------
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s){	
+
+	// your code here
+	
+    return s;	
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments. 
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE * file){
+	
+	// your code here
+	
+}
