84036f588eb945e97b96015b81ee775084b85ac1
Liam Colburn <colburnw@oregonstate.edu>
2023-10-24 11:06:55 -0700

    Set up main

--- cploration/c04/main.c
+++ cploration/c04/main.c
@@ -1,17 +1,31 @@
 /****************************************
  * C-ploration 4 for CS 271
  * 
- * [NAME] $YOUR_NAME$
- * [TERM] FALL $YEAR$
+ * [NAME] William Colburn
+ * [TERM] FALL 2023
  * 
  ****************************************/
 #include "parser.h"
 
 int main(int argc, const char *argv[])
-{		
+{
 
-	// your code here
-			
+    if (argc != 2) {
+        // if incorrect number of arguments
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fin = fopen( argv[1], "r" );
+    if (fin == NULL) {
+        perror("Unable to open file!");
+        exit(EXIT_FAILURE);
+    }
+
+    parse(fin);
+
+    fclose(fin);
+
 }
 
 
