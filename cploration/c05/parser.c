304aa1c7b4e3056a025a9f588b1b437f3e88297d
Liam Colburn <colburnw@oregonstate.edu>
2023-11-02 10:17:49 -0700

    Init commit

---
+++ cploration/c05/parser.c
@@ -0,0 +1,58 @@
+/****************************************
+ * C-ploration 5 for CS 271
+ * 
+ * [NAME] William Colburn
+ * [TERM] FALL 2023
+ * 
+ ****************************************/
+#include "parser.h"
+
+/* Function: strip
+ * -------------
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s) {
+
+    char s_new[MAX_LINE_LENGTH];
+    int i = 0;
+
+    for (char *s2 = s; *s2; s2++) {
+
+        if (*s2 == '/' && *(s2+1) == '/') {
+            break;
+        } else if (!isspace(*s2)) {
+            s_new[i++] = *s2;
+        }
+    }
+
+    s_new[i] = '\0';
+    strcpy(s, s_new);
+
+    return s;	
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments. 
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE * file){
+
+    char line[MAX_LINE_LENGTH];
+
+    while (fgets(line, sizeof(line), file)) {
+        strip(line);
+        if (!*line) {
+            continue;
+        }
+        printf("%s\n", line);
+    }
+	
+}
---
+++ cploration/c05/parser.h
@@ -0,0 +1,18 @@
+/****************************************
+ * C-ploration 4 for CS 271
+ * 
+ * [NAME] William Colburn
+ * [TERM] FALL 2023
+ * 
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_LINE_LENGTH  200
+
+/** function prototypes **/
+char *strip(char *s);
+
+void parse(FILE * file);
