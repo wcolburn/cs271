64f64e7ea0983d38b6984c27bffae9faa07f3f68
Liam Colburn <colburnw@oregonstate.edu>
2023-11-14 10:30:29 -0800

    Define symtable API

--- cploration/c07/symtable.h
+++ cploration/c07/symtable.h
@@ -9,10 +9,14 @@
 #include <string.h>
 
 const int SYMBOL_TABLE_SIZE = 1000;
-
 typedef int16_t hack_addr;
-
 typedef struct Symbol {
     char *name;
     hack_addr addr;
 } Symbol;
+
+int hash(char *str);
+struct Symbol *symtable_find(char *key);
+void symtable_insert(char* key, hack_addr addr);
+void symtable_display_table();
+void symtable_print_labels();
