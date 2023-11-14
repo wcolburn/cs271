9f71ede42e3c9ba75e9bc640ae3d0b7054587c40
Liam Colburn <colburnw@oregonstate.edu>
2023-11-14 10:24:13 -0800

    Start symtable.h

---
+++ cploration/c07/symtable.h
@@ -0,0 +1,13 @@
+/****************************************
+ * C-ploration 7 for CS 271
+ *
+ * [NAME] William Colburn
+ * [TERM] FALL 2023
+ *
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+
+const int SYMBOL_TABLE_SIZE = 1000;
+
+typedef int16_t hack_addr;
