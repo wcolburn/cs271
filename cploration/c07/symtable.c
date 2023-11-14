5d745e624ae471b50aa4a178595692f4085e427d
Liam Colburn <colburnw@oregonstate.edu>
2023-11-14 10:48:23 -0800

    Implement hash function

--- cploration/c07/symtable.c
+++ cploration/c07/symtable.c
@@ -8,3 +8,14 @@
 #include "symtable.h"
 
 Symbol* hashArray[SYMBOL_TABLE_SIZE];
+
+int hash(char *string) {
+    int hash = 5381;
+    int c;
+
+    while ((c = *string++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    hash = hash % SYMBOL_TABLE_SIZE;
+    return hash;
+}
