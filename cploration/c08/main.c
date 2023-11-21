cf7f543e5f2ba9196e235c911a0e31ed756593aa
Liam Colburn <colburnw@oregonstate.edu>
2023-11-21 10:03:32 -0800

    Init commit

---
+++ cploration/c08/main.c
@@ -0,0 +1,32 @@
+/****************************************
+ * C-ploration 7 for CS 271
+ * 
+ * [NAME] William Colburn
+ * [TERM] FALL 2023
+ * 
+ ****************************************/
+#include "parser.h"
+//#include "symtable.h"
+
+int main(int argc, const char *argv[])
+{
+
+    if (argc != 2) {
+        // if incorrect number of arguments
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fin = fopen( argv[1], "r" );
+    if (fin == NULL) {
+        perror("Unable to open file!");
+        exit(EXIT_FAILURE);
+    }
+
+    parse(fin);
+    symtable_print_labels();
+    fclose(fin);
+
+}
+
+
---
+++ cploration/c08/symtable.h
@@ -0,0 +1,23 @@
+/****************************************
+ * C-ploration 7 for CS 271
+ *
+ * [NAME] William Colburn
+ * [TERM] FALL 2023
+ *
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SYMBOL_TABLE_SIZE 1000
+typedef int16_t hack_addr;
+typedef struct Symbol {
+    char *name;
+    hack_addr addr;
+} Symbol;
+
+int hash(char *str);
+Symbol *symtable_find(char * key);
+void symtable_insert(char* key, hack_addr addr);
+void symtable_display_table();
+void symtable_print_labels();
