5830fcc1f749ea1276fb9d0f30ae6d752792fcc9
Liam Colburn <colburnw@oregonstate.edu>
2023-11-21 11:07:35 -0800

    Start error header file

--- cploration/c08/error.h
+++ cploration/c08/error.h
@@ -5,10 +5,18 @@
  * [TERM] FALL 2023
  *
  ****************************************/
-
+#include <stdarg.h>
 
 #ifndef __ERROR_H__
 #define __ERROR_H__
 
+enum exitcode {
+    /*
+    * Error code 1 represents that given file does not exist.
+    */
+    EXIT_INCORRECT_ARGUMENTS = 1
+};
+
+void exit_program(enum exitcode code, ...);
 
 #endif
