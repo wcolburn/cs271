ee3b03a9dfe9fc263fba284b7796850c08f28645
Liam Colburn <colburnw@oregonstate.edu>
2023-11-28 10:30:56 -0800

    Create predefined symbol struct

--- cploration/c09/asm/hack.h
+++ cploration/c09/asm/hack.h
@@ -32,11 +32,11 @@ enum symbol_id {
     SYM_R15,
     SYM_SCREEN = 16384,
     SYM_KBD = 24576,
-    SP = 0,
-    LCL,
-    ARG,
-    THIS,
-    THAT
+    SYM_SP = 0,
+    SYM_LCL,
+    SYM_ARG,
+    SYM_THIS,
+    SYM_THAT
 };
 
 typedef struct {
@@ -44,5 +44,30 @@ typedef struct {
     int16_t address;
 } predefined_symbol;
 
+static const predefined_symbol predefined_symbols[NUM_PREDEFINED_SYMBOLS] = {
+        {"R0", SYM_R0},
+        {"R1", SYM_R1},
+        {"R2", SYM_R2},
+        {"R3", SYM_R3},
+        {"R4", SYM_R4},
+        {"R5", SYM_R5},
+        {"R6", SYM_R6},
+        {"R7", SYM_R7},
+        {"R8", SYM_R8},
+        {"R9", SYM_R9},
+        {"R10", SYM_R10},
+        {"R11", SYM_R11},
+        {"R12", SYM_R12},
+        {"R13", SYM_R13},
+        {"R14", SYM_R14},
+        {"R15", SYM_R15},
+        {"SCREEN", SYM_SCREEN},
+        {"SP", SYM_SP},
+        {"KBD", SYM_KBD},
+        {"LCL", SYM_LCL},
+        {"ARG", SYM_ARG},
+        {"THIS", SYM_THIS},
+        {"THAT", SYM_THAT}
+};
 
 #endif
